bb0678c6dbf1b55d691bdf3c51a6980ed6d95284
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-09-19 19:15:52 +0200

    add new tests

--- models/product.model.js
+++ models/product.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    client: { type: String, required: true }
+  name: { type: String, required: true },
+  client: { type: String, required: true }
 });
 
 module.exports = mongoose.model('product', ProductSchema);
---
+++ test/api/departments/get.test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../server.js');
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('GET /api/departments', () => {
+
+    it('/ should return all departments', () => {
+
+    });
+
+    it('/:id should return one department by :id ', () => {
+
+    });
+
+    it('/random should return one random department', () => {
+
+    });
+
+});
